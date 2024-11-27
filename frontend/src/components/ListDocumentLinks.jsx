ae09232cc77d0dec6db68e5cf625d94208c98476
Giorgio Rondinone <s332156@studenti.polito.it>
2024-11-27 16:28:53 +0100

    Adjustments

--- frontend/src/components/ListDocumentLinks.jsx
+++ frontend/src/components/ListDocumentLinks.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Card, Row, Col } from 'react-bootstrap';
+import { Button, Card, Row, Col, ListGroup } from 'react-bootstrap';
 import API from '../API.mjs';
 import '../App.css';
 
@@ -8,7 +8,7 @@ const ListDocumentLinks = ({ documentId, isOpen, onClose, onSnippetClick }) => {
 
   useEffect(() => {
     if (isOpen) {
-      API.getAllDocumentSnippets()
+      API.getAllLinksOfDocument(documentId)
         .then((response) => {
           setSnippets(response);
         })
@@ -33,23 +33,36 @@ const ListDocumentLinks = ({ documentId, isOpen, onClose, onSnippetClick }) => {
     <div className={`slider ${isOpen ? 'open' : ''}`}>
       <div className="snippet-list">
         <Row xs={1} className="g-4" style={{ width: "100%" }}>
-          {snippets.map((snippet) => (
-            <Col key={snippet.id}>
+          {snippets.map((snippet, index) => (
+            <Col key={index}>
               <Card className="document-card slider-card" onClick={() => handleSnippetClick(snippet)}>
                 <Card.Body>
                   <Card.Title className="document-card-title">
-                    {snippet.title}
+                    {snippet.document.title}
                   </Card.Title>
                   <div className="divider" />
                   <Card.Text className="document-card-text">
-                    <strong>Scale:</strong> {snippet.scale}
+                    <strong>Scale:</strong> {snippet.document.scale}
                   </Card.Text>
                   <Card.Text className="document-card-text">
-                    <strong>Issuance Date:</strong> {snippet.issuanceDate}
+                    <strong>Issuance Date:</strong> {snippet.document.issuanceDate}
                   </Card.Text>
                   <Card.Text className="document-card-text">
-                    <strong>Type:</strong> {snippet.type}
+                    <strong>Type:</strong> {snippet.document.type}
                   </Card.Text>
+                  <Card.Text className="document-card-text">
+                    <strong>Stakeholders:</strong> {snippet.document.stakeholders.join(', ')}
+                  </Card.Text>
+                  <Card.Text className="document-card-text">
+                    <strong>Links:</strong>
+                  </Card.Text>
+                  <ListGroup>
+                    {snippet.links.map((link, idx) => (
+                      <ListGroup.Item key={idx}>
+                        {link.linkType}
+                      </ListGroup.Item>
+                    ))}
+                  </ListGroup>
                 </Card.Body>
               </Card>
             </Col>
