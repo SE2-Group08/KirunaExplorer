e1acd9406179c8226e4095826db8c520267085ff
Giorgio Rondinone <s332156@studenti.polito.it>
2024-11-27 12:00:35 +0100

    FE part authentication

--- frontend/src/API.mjs
+++ frontend/src/API.mjs
@@ -1,38 +1,97 @@
 import { Document, DocumentSnippet } from "./model/Document.mjs";
+import { User } from "./model/User.mjs";
 import Stakeholder from "./model/Stakeholder.mjs";
 
 const SERVER_URL = "http://localhost:8080/api/v1";
 
-/* ************************** *
- *       Link APIs      *
- * ************************** */
+/* **************************** *
+ *      Authentication APIs     *
+ * **************************** */
+
+// Given a credentials object containing username and passowrd it executes login
+const logIn = async (credentials) => {
+  console.log("API.logIn", credentials);
+  return await fetch(SERVER_URL + "/sessions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify(credentials),
+  })
+    .then(handleInvalidResponse)
+    .then((response) => response.json());
+};
+
+// Verifies if user is still logged-in. It returns a JSON with the user info
+const getUserInfo = async () => {
+  return await fetch(SERVER_URL + "/sessions/current", {
+    credentials: "include",
+  })
+    .then(handleInvalidResponse)
+    .then((response) => response.json);
+};
+
+// Destroys the current user's session (executing log-out)
+const logOut = async () => {
+  return await fetch(SERVER_URL + "/sessions/current", {
+    method: "DELETE",
+    credentials: "include",
+  }).then(handleInvalidResponse);
+};
+
+/* ********************* *
+ *       User APIs       *
+ * ********************* */
+
+const getUsers = async () => {
+  return await fetch(SERVER_URL + "/users")
+    .then(handleInvalidResponse)
+    .then((response) => response.json())
+    .then((jsonArray) => jsonArray.map(User.fromJson));
+};
+
+const getUserById = async (userId) => {
+  return await fetch(`${SERVER_URL}/users/${userId}`)
+    .then(handleInvalidResponse)
+    .then((response) => response.json())
+    .then(User.fromJson);
+};
+
+/* ********************* *
+ *       Link APIs       *
+ * ********************* */
 
 const createLink = async (document, linkedDocument) => {
-  console.log("CREATE LINK: ", document, linkedDocument);
   const requestBody = {
     type: linkedDocument.linkType.toUpperCase(),
     linkId: null,
     documentId: linkedDocument.document.id,
   };
-  console.log("REQUEST BODY: ", requestBody);
-
-  // ("REQUEST BODY: ", requestBody);
   requestBody.type = linkedDocument.linkType.toUpperCase().replace(/ /g, "_");
-  console.log(document.id)
   try {
-    const response = await fetch(`${SERVER_URL}/documents/${document.id}/links`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(requestBody)
-    });
+    const response = await fetch(
+      `${SERVER_URL}/documents/${document.id}/links`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify(requestBody),
+      }
+    );
 
     if (response.ok) {
-      const responseData = response.status !== 201 ? await response.json() : null;
+      const responseData =
+        response.status !== 201 ? await response.json() : null;
       console.log("Link creato con successo:", responseData);
     } else {
-      console.error("Errore nella creazione del link:", response.status, response.statusText);
+      console.error(
+        "Errore nella creazione del link:",
+        response.status,
+        response.statusText
+      );
     }
   } catch (error) {
     console.error("Errore nella richiesta:", error);
@@ -41,7 +100,9 @@ const createLink = async (document, linkedDocument) => {
 
 // Retrieve all links of a document
 const getAllLinksOfDocument = async (documentId) => {
-  const links = await fetch(`${SERVER_URL}/api/v1/documents/${documentId}/links`)
+  const links = await fetch(
+    `${SERVER_URL}/api/v1/documents/${documentId}/links`
+  )
     .then(handleInvalidResponse)
     .then((response) => response.json());
   return links;
@@ -54,15 +115,20 @@ const updateLink = async (documentId, linkId, updatedLink) => {
     headers: {
       "Content-Type": "application/json",
     },
+    credentials: "include",
     body: JSON.stringify(updatedLink),
   }).then(handleInvalidResponse);
 };
 
 // Delete a link for a document
 const deleteLink = async (documentId, linkId) => {
-  return await fetch(`${SERVER_URL}/api/v1/documents/${documentId}/links/${linkId}`, {
-    method: "DELETE",
-  }).then(handleInvalidResponse);
+  return await fetch(
+    `${SERVER_URL}/api/v1/documents/${documentId}/links/${linkId}`,
+    {
+      method: "DELETE",
+      credentials: "include",
+    }
+  ).then(handleInvalidResponse);
 };
 
 /* ************************** *
@@ -74,7 +140,7 @@ const getAllDocumentSnippets = async (filter) => {
   const documents = await fetch(`${SERVER_URL}/documents`)
     .then(handleInvalidResponse)
     .then((response) => response.json())
-    .then(mapAPISnippetsToSnippet);
+    .then((jsonArray) => jsonArray.map(DocumentSnippet.fromJson));
   return documents;
 };
 
@@ -86,6 +152,7 @@ const addDocument = async (document) => {
     headers: {
       "Content-Type": "application/json",
     },
+    credentials: "include",
     body: JSON.stringify(document),
   }).then(handleInvalidResponse);
 };
@@ -95,7 +162,7 @@ const getDocumentById = async (documentId) => {
   const document = await fetch(`${SERVER_URL}/documents/${documentId}`)
     .then(handleInvalidResponse)
     .then((response) => response.json())
-    .then(mapAPIDocumentToDocument);
+    .then(Document.fromJson);
   return document;
 };
 
@@ -106,6 +173,7 @@ const updateDocument = async (documentId, nextDocument) => {
     headers: {
       "Content-Type": "application/json",
     },
+    credentials: "include",
     body: JSON.stringify(nextDocument),
   }).then(handleInvalidResponse);
 };
@@ -114,6 +182,7 @@ const updateDocument = async (documentId, nextDocument) => {
 const deleteDocument = async (documentId) => {
   return await fetch(`${SERVER_URL}/documents/${documentId}`, {
     method: "DELETE",
+    credentials: "include",
   }).then(handleInvalidResponse);
 };
 
@@ -182,43 +251,6 @@ function handleInvalidResponse(response) {
   return response;
 }
 
-function mapAPIStakeholdersToStakeholders(apiStakeholders) {
-  return apiStakeholders.map(
-    (apiStakeholder) => new Stakeholder(apiStakeholder.id, apiStakeholder.name)
-  );
-}
-
-async function mapAPIDocumentToDocument(apiDocument) {
-  return new Document(
-    apiDocument.id,
-    apiDocument.title,
-    apiDocument.stakeholders,
-    apiDocument.scale,
-    apiDocument.issuanceDate,
-    apiDocument.type,
-    apiDocument.nrConnections,
-    apiDocument.language,
-    apiDocument.nrPages,
-    apiDocument.geolocation,
-    apiDocument.description
-  );
-}
-
-async function mapAPISnippetsToSnippet(apiSnippets) {
-  return apiSnippets.map(
-    (apiSnippet) =>
-      new DocumentSnippet(
-        apiSnippet.id,
-        apiSnippet.title,
-        apiSnippet.scale,
-        apiSnippet.issuanceDate,
-        apiSnippet.type,
-        apiSnippet.geolocation,
-        apiSnippet.stakeholders
-      )
-  );
-}
-
 const API = {
   getAllDocumentSnippets,
   addDocument,
@@ -234,5 +266,10 @@ const API = {
   getAllLinksOfDocument,
   updateLink,
   deleteLink,
+  logIn,
+  getUserInfo,
+  logOut,
+  getUsers,
+  getUserById,
 };
 export default API;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,12 +7,31 @@ import ListDocuments from "./components/ListDocuments";
 import SplashPage from "./components/SplashPage";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Map from "./components/Map";
+import { LoginComponent } from "./components/LoginPage";
 import "./App.css";
+import { useState } from "react";
+import API from "./API";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = async (credentials) => {
+    console.log("App.handleLogin", credentials);
+    const user = await API.logIn(credentials);
+    setUser(user);
+    setLoggedIn(true);
+  }
+  
+  const handleLogout = async () => {
+    await API.logOut();
+    setLoggedIn(false);
+    setUser(null);
+  }
+
   return (
     <div>
-      <Header />
+      <Header loggedIn={loggedIn} userInfo={user} logout={handleLogout}/>
       <Container fluid className="d-flex flex-column min-vh-100 p-0 mt-5">
         <Routes>
           <Route
@@ -24,6 +43,7 @@ function App() {
           >
             <Route path="/documents" element={<ListDocuments />} />
             <Route path="/map" element={<Map />} />
+            <Route path="/login" element={<LoginComponent login={handleLogin}/>} />
             <Route path="/" element={<SplashPage />} />
             <Route
               path="*"
