ebd01c74999d8288af0b645e362af7806631f7f0
Alessandro Fedriga <s320136@studenti.polito.it>
2024-11-29 20:19:47 +0100

    Update of the document service layer to account for stakeholder repository

--- backend/src/main/java/com/kirunaexplorer/app/service/DocumentService.java
+++ backend/src/main/java/com/kirunaexplorer/app/service/DocumentService.java
@@ -6,9 +6,12 @@ import com.kirunaexplorer.app.dto.response.DocumentResponseDTO;
 import com.kirunaexplorer.app.exception.ResourceNotFoundException;
 import com.kirunaexplorer.app.model.Document;
 import com.kirunaexplorer.app.model.GeoReference;
+import com.kirunaexplorer.app.model.Stakeholder;
 import com.kirunaexplorer.app.repository.DocumentLinkRepository;
 import com.kirunaexplorer.app.repository.DocumentRepository;
 import com.kirunaexplorer.app.repository.GeoReferenceRepository;
+import com.kirunaexplorer.app.repository.StakeholderRepository;
+import com.kirunaexplorer.app.util.DocumentFieldsChecker;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,15 +22,23 @@ public class DocumentService {
     private final DocumentRepository documentRepository;
     private final GeoReferenceRepository geoReferenceRepository;
     private final DocumentLinkRepository documentLinkRepository;
+    private final StakeholderRepository stakeholderRepository;
 
-    public DocumentService(DocumentRepository documentRepository, GeoReferenceRepository geoReferenceRepository, DocumentLinkRepository documentLinkRepository) {
+    public DocumentService(
+        DocumentRepository documentRepository,
+        GeoReferenceRepository geoReferenceRepository,
+        DocumentLinkRepository documentLinkRepository,
+        StakeholderRepository stakeholderRepository
+    ) {
         this.geoReferenceRepository = geoReferenceRepository;
         this.documentRepository = documentRepository;
         this.documentLinkRepository = documentLinkRepository;
+        this.stakeholderRepository = stakeholderRepository;
     }
 
-    /***
+    /**
      * Get all documents in brief format
+     *
      * @return List of DocumentBriefResponseDTO
      */
     public List<DocumentBriefResponseDTO> getAllDocuments() {
@@ -36,8 +47,9 @@ public class DocumentService {
             .toList();
     }
 
-    /***
+    /**
      * Get a document by id
+     *
      * @param id Document id
      * @return DocumentResponseDTO
      */
@@ -48,33 +60,63 @@ public class DocumentService {
             .toResponseDTO(documentLinkRepository.countByDocumentId(id));
     }
 
-    /***
+    /**
      * Create a document
+     *
      * @param documentRequest DocumentRequestDTO
      * @return DocumentRequestDTO
      */
     @Transactional
     public Long createDocument(DocumentRequestDTO documentRequest) {
+        // Remove duplicates
+        DocumentFieldsChecker.removeStakeholderDuplicates(documentRequest);
+
+        // Get existing stakeholders
+        List<Stakeholder> existingStakeholders = stakeholderRepository.findAll();
+
+        // Get new stakeholders to add to the database
+        List<Stakeholder> newStakeholders = DocumentFieldsChecker.getNewStakeholders(documentRequest.stakeholders(), existingStakeholders);
+
+        // Add new stakeholders to the database
+        stakeholderRepository.saveAll(newStakeholders);
+
+        // Save document
         Document document = documentRequest.toDocument();
         document = documentRepository.save(document);
 
+        // Save geolocation
         GeoReference geoReference = documentRequest.geolocation().toGeoReference(document);
         geoReferenceRepository.save(geoReference);
 
         return document.getId();
     }
 
-    /***
+    /**
      * Update a document
+     *
      * @param documentRequest DocumentRequestDTO
      */
     @Transactional
     public void updateDocument(DocumentRequestDTO documentRequest) {
+        // Get document by id
         Long id = documentRequest.id();
         Document document = documentRepository.findById(id)
             .orElseThrow(() -> new ResourceNotFoundException("Document not found with ID " + id));
 
-        document.updateFromDTO(documentRequest); // Update fields
+        // Remove duplicates
+        DocumentFieldsChecker.removeStakeholderDuplicates(documentRequest);
+
+        // Get existing stakeholders
+        List<Stakeholder> existingStakeholders = stakeholderRepository.findAll();
+
+        // Get new stakeholders to add to the database
+        List<Stakeholder> newStakeholders = DocumentFieldsChecker.getNewStakeholders(documentRequest.stakeholders(), existingStakeholders);
+
+        // Add new stakeholders to the database
+        stakeholderRepository.saveAll(newStakeholders);
+
+        // Update document
+        document.updateFromDTO(documentRequest);
         documentRepository.save(document);
 
         GeoReference geoReference = geoReferenceRepository.findById(document.getId())
@@ -83,5 +125,7 @@ public class DocumentService {
         geoReference.updateFromDTO(documentRequest.geolocation()); // Update geolocation
         geoReferenceRepository.save(geoReference);
     }
+
+
 }
 
---
+++ backend/src/main/java/com/kirunaexplorer/app/util/DocumentFieldsChecker.java
@@ -0,0 +1,40 @@
+package com.kirunaexplorer.app.util;
+
+import com.kirunaexplorer.app.dto.request.DocumentRequestDTO;
+import com.kirunaexplorer.app.dto.request.StakeholderRequestDTO;
+import com.kirunaexplorer.app.model.Stakeholder;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class DocumentFieldsChecker {
+
+    /**
+     * Remove duplicates from the list of stakeholders
+     *
+     * @param document Document to check
+     */
+    public static void removeStakeholderDuplicates(DocumentRequestDTO document) {
+        List<String> uniqueStakeholders = document.stakeholders().stream()
+            .distinct()
+            .toList();
+        document.stakeholders().clear();
+        document.stakeholders().addAll(uniqueStakeholders);
+    }
+
+    /**
+     * Get new stakeholders from the list of stakeholders
+     *
+     * @param stakeholders         List of stakeholders
+     * @param existingStakeholders List of existing stakeholders
+     * @return List of new stakeholders
+     */
+    public static List<Stakeholder> getNewStakeholders(List<String> stakeholders, List<Stakeholder> existingStakeholders) {
+        return stakeholders.stream()
+            .filter(stakeholder -> existingStakeholders.stream().noneMatch(existing -> existing.getName().equals(stakeholder)))
+            .map(name -> new StakeholderRequestDTO(null, name).toStakeholder())
+            .collect(Collectors.toList());
+    }
+}
