b5cf77393cce390a612ce07ad9c894f198bc875d
Nassim MANSOUR <mansournassim7@gmail.com>
2024-12-04 18:00:56 +0100

    Update document repository

--- backend/src/main/java/com/kirunaexplorer/app/repository/DocumentRepository.java
+++ backend/src/main/java/com/kirunaexplorer/app/repository/DocumentRepository.java
@@ -2,8 +2,16 @@ package com.kirunaexplorer.app.repository;
 
 import com.kirunaexplorer.app.model.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DocumentRepository extends JpaRepository<Document, Long> {
+
+    @Query("SELECT d FROM Document d WHERE (LOWER(d.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(d.description) LIKE LOWER(CONCAT('%', :keyword, '%'))) AND (:type IS NULL OR d.type = :type)")
+    List<Document> searchDocuments(@Param("keyword") String keyword, @Param("type") String type);
 }
