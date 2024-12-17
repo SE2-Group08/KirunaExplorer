033bb626868d8e771eeed2c1d0056f3e54ee532e
Alessandro Fedriga <s320136@studenti.polito.it>
2024-12-17 12:02:03 +0100

    added some documents

--- backend/src/main/java/com/kirunaexplorer/app/config/DataInitializer.java
+++ backend/src/main/java/com/kirunaexplorer/app/config/DataInitializer.java
@@ -46,29 +46,39 @@ public class DataInitializer implements CommandLineRunner {
     @Override
     @Transactional
     public void run(String... args) throws Exception {
-//        initializeStakeholders();
-//        initializeDocumentTypes();
-//        initializeDocumentScales();
+        initializeStakeholders();
+        initializeDocumentTypes();
+        initializeDocumentScales();
 
         // Initialize Areas
         Area area1 = areaRepository.save(new Area(
             null,
             "AAAAAAAA",
             new Coordinates(69.0, 20.0),
-            new Geometry(GeometryType.POLYGON, List.of(new Coordinates(69.0, 20.0), new Coordinates(69.0, 21.0),
-                new Coordinates(70.0, 21.0), new Coordinates(70.0, 20.0)))));
+            new Geometry(GeometryType.POLYGON, List.of(new Coordinates(69.0, 20.0), new Coordinates(69.0, 21.0), new Coordinates(70.0, 21.0), new Coordinates(70.0, 20.0)))
+        ));
 
+        Area area2 = areaRepository.save(new Area(
+            null,
+            "BBBBB",
+            new Coordinates(68.0, 21.0),
+            new Geometry(GeometryType.POLYGON, List.of(new Coordinates(68.0, 21.0), new Coordinates(68.5, 21.5), new Coordinates(68.8, 21.8)))
+        ));
+
+        // Initialize Point Coordinates
         PointCoordinates point1 = pointCoordinatesRepository.save(new PointCoordinates(
             null,
             "Cool new point",
             69.0,
-            20.0));
+            20.0
+        ));
 
         PointCoordinates point2 = pointCoordinatesRepository.save(new PointCoordinates(
             null,
             "Cool new point 2",
-            69.0,
-            20.0));
+            68.5,
+            20.5
+        ));
 
         PointCoordinates point3 = pointCoordinatesRepository.save(new PointCoordinates(
             null,
@@ -97,6 +107,66 @@ public class DataInitializer implements CommandLineRunner {
             document1,
             null,
             point3));
+
+        Document document2 = documentRepository.save(new Document(null,
+            "Detailed plan for Bolagsomradet Gruvstadspark",
+            "Detailed transformation of residential areas into mining zones.",
+            "Kiruna kommun",
+            "Prescriptive document",
+            "1:8000",
+            LocalDate.of(2010, 10, 20),
+            Document.DatePrecision.FULL_DATE,
+            "Swedish",
+            32,
+            LocalDateTime.now(),
+            null,
+            new HashSet<>(),
+            null,
+            new HashSet<>()
+        ));
+
+        geoReferenceRepository.save(new GeoReference(
+            document2,
+            null,
+            point1));
+
+        Document document3 = documentRepository.save(new Document(null,
+            "Development Plan",
+            "Shaping the new city through strategies and diagrams.",
+            "Kiruna kommun/White Arkitekter",
+            "Design document",
+            "1:7500",
+            LocalDate.of(2014, 3, 17),
+            Document.DatePrecision.FULL_DATE,
+            "Swedish",
+            111,
+            LocalDateTime.now(),
+            null,
+            new HashSet<>(),
+            null,
+            new HashSet<>()
+        ));
+
+        geoReferenceRepository.save(new GeoReference(document3, area2, null));
+
+        Document document4 = documentRepository.save(new Document(null,
+            "Deformation forecast",
+            "Forecast for deformation analysis in Kiruna region.",
+            "LKAB",
+            "Technical document",
+            "1:12000",
+            LocalDate.of(2014, 12, 1),
+            Document.DatePrecision.FULL_DATE,
+            "Swedish",
+            1,
+            LocalDateTime.now(),
+            null,
+            new HashSet<>(),
+            null,
+            new HashSet<>()
+        ));
+
+        geoReferenceRepository.save(new GeoReference(document4, null, point2));
 
         // initializeStakeholders();
         // initializeDocumentType();
@@ -404,23 +474,23 @@ public class DataInitializer implements CommandLineRunner {
         // Add more documents and GeoReferences as needed
     }
 
-//    private void initializeStakeholders() {
-//        stakeholderRepository.save(new Stakeholder(null, "Kiruna kommun"));
-//        stakeholderRepository.save(new Stakeholder(null, "Residents"));
-//        stakeholderRepository.save(new Stakeholder(null, "White Arkitekter"));
-//        stakeholderRepository.save(new Stakeholder(null, "LKAB"));
-//    }
-//
-//    private void initializeDocumentTypes() {
-//        documentTypeRepository.save(new DocumentType(null, "Informative document"));
-//        documentTypeRepository.save(new DocumentType(null, "Design document"));
-//        documentTypeRepository.save(new DocumentType(null, "Technical document"));
-//        documentTypeRepository.save(new DocumentType(null, "Prescriptive document"));
-//        documentTypeRepository.save(new DocumentType(null, "Material effect"));
-//    }
-//
-//    private void initializeDocumentScales() {
-//        documentScaleRepository.save(new DocumentScale(null, "Text"));
-//        documentScaleRepository.save(new DocumentScale(null, "Blueprints/Material effects"));
-//    }
+    private void initializeStakeholders() {
+        stakeholderRepository.save(new Stakeholder(null, "Kiruna kommun"));
+        stakeholderRepository.save(new Stakeholder(null, "Residents"));
+        stakeholderRepository.save(new Stakeholder(null, "White Arkitekter"));
+        stakeholderRepository.save(new Stakeholder(null, "LKAB"));
+    }
+
+    private void initializeDocumentTypes() {
+        documentTypeRepository.save(new DocumentType(null, "Informative document"));
+        documentTypeRepository.save(new DocumentType(null, "Design document"));
+        documentTypeRepository.save(new DocumentType(null, "Technical document"));
+        documentTypeRepository.save(new DocumentType(null, "Prescriptive document"));
+        documentTypeRepository.save(new DocumentType(null, "Material effect"));
+    }
+
+    private void initializeDocumentScales() {
+        documentScaleRepository.save(new DocumentScale(null, "Text"));
+        documentScaleRepository.save(new DocumentScale(null, "Blueprints/Material effects"));
+    }
 }
