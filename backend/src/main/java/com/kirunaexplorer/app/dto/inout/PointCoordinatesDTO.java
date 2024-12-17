8f25f0aca79f40a97b688e64656607fb58078598
Alessandro Fedriga <s320136@studenti.polito.it>
2024-12-17 12:59:27 +0100

    added static validation for geolocation
    POST and PUT /api/v1/documents

--- backend/src/main/java/com/kirunaexplorer/app/dto/inout/PointCoordinatesDTO.java
+++ backend/src/main/java/com/kirunaexplorer/app/dto/inout/PointCoordinatesDTO.java
@@ -1,20 +1,25 @@
 package com.kirunaexplorer.app.dto.inout;
 
 import com.kirunaexplorer.app.model.PointCoordinates;
+import com.kirunaexplorer.app.validation.groups.document.PostDocument;
+import com.kirunaexplorer.app.validation.groups.document.PutDocument;
 import com.kirunaexplorer.app.validation.groups.point_coordinates.PostPointCoordinates;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
 
 public record PointCoordinatesDTO(
     @Null(groups = {PostPointCoordinates.class})
+    @NotNull(groups = {PostDocument.class, PutDocument.class})
     Long pointId,
 
-    @NotNull
+    @NotNull(groups = {PostPointCoordinates.class})
+    @Null(groups = {PostDocument.class, PutDocument.class})
     @Size(min = 2, max = 64, groups = {PostPointCoordinates.class})
     String pointName,
 
     @Valid
     @NotNull(groups = {PostPointCoordinates.class})
+    @Null(groups = {PostDocument.class, PutDocument.class})
     CoordinatesDTO coordinates
 ) {
 
--- backend/src/main/java/com/kirunaexplorer/app/validation/annotation/ValidGeoReference.java
+++ backend/src/main/java/com/kirunaexplorer/app/validation/annotation/ValidGeoReference.java
@@ -1,6 +1,6 @@
 package com.kirunaexplorer.app.validation.annotation;
 
-import com.kirunaexplorer.app.validation.validator.OneOfGeoReferenceValidator;
+import com.kirunaexplorer.app.validation.validator.GeoReferenceValidator;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
@@ -9,11 +9,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Constraint(validatedBy = GeoReferenceValidator.class)
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = OneOfGeoReferenceValidator.class)
-public @interface OneOfGeoReference {
-    String message() default "Geolocation must match one of the specified formats";
+public @interface ValidGeoReference {
+    String message() default "Both area and pointCoordinates cannot have values at the same time";
 
     Class<?>[] groups() default {};
 
