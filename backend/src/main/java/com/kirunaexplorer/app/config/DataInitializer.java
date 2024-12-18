0cb0361dc6dfeaad791dc13859dd67a9497ac0d8
Nassim MANSOUR <mansournassim7@gmail.com>
2024-12-18 20:47:35 +0100

    Update Data Initializer

--- backend/src/main/java/com/kirunaexplorer/app/config/DataInitializer.java
+++ backend/src/main/java/com/kirunaexplorer/app/config/DataInitializer.java
@@ -13,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashSet;
+import java.util.List;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
@@ -45,15 +46,24 @@ public class DataInitializer implements CommandLineRunner {
     @Transactional
     public void run(String... args) throws Exception {
 
-        initializeStakeholders();
         initializeDocumentType();
         initializeDocumentScale();
+
+        var kirunaKommunStakeholder = new Stakeholder("Kiruna kommun");
+        var residentsStakeholder = new Stakeholder("Residents");
+        var whiteArkitekterStakeholder = new Stakeholder("White Arkitekter");
+        var lkabStakeholder = new Stakeholder("LKAB");
+
+        stakeholderRepository.save(kirunaKommunStakeholder);
+        stakeholderRepository.save(residentsStakeholder);
+        stakeholderRepository.save(whiteArkitekterStakeholder);
+        stakeholderRepository.save(lkabStakeholder);
 
 
         Document document1 = new Document(null,
             "Compilation of responses 'So what the people of Kiruna think?'",
             "This document is a compilation of the responses to the survey 'What is your impression of Kiruna?' From the citizens' responses to this last part of the survey, it is evident that certain buildings, such as the Kiruna Church, the Hjalmar Lundbohmsgärden, and the Town Hall, are considered of significant value to the population. The municipality views the experience of this survey positively, to the extent that over the years it will propose various consultation opportunities.",
-            "Kiruna kommun/Residents",
+            List.of(kirunaKommunStakeholder, residentsStakeholder),
             "Informative document",
             "1:1",
             LocalDate.of(2007, 1, 1),
@@ -80,7 +90,7 @@ public class DataInitializer implements CommandLineRunner {
         Document document2 = new Document(null,
             "Detailed plan for Bolagsomradet Gruvstadspark",
             "This is the first of 8 detailed plans located in the old center of Kiruna, aimed at transforming the residential areas into mining industry zones to allow the demolition of buildings. The area includes the town hall, the Ullspiran district, and the AlO highway, and it will be the first to be dismantled. The plan consists, like all detailed plans, of two documents: the area map that regulates it, and a text explaining the reasons that led to the drafting of the plan with these characteristics. The plan gained legal validity in 2012.",
-            "Kiruna kommun",
+            List.of(kirunaKommunStakeholder),
             "Prescriptive document",
             "1:8000",
             LocalDate.of(2010, 10, 20),
@@ -105,7 +115,7 @@ public class DataInitializer implements CommandLineRunner {
         Document document3 = new Document(null,
             "Development Plan",
             "The development plan shapes the form of the new city. The document, unlike previous competition documents, is written entirely in Swedish, which reflects the target audience: the citizens of Kiruna. The plan obviously contains many elements of the winning masterplan from the competition, some recommended by the jury, and others that were deemed appropriate to integrate later. The document is divided into four parts, with the third part, spanning 80 pages, describing the shape the new city will take and the strategies to be implemented for its relocation through plans, sections, images, diagrams, and texts. The document also includes numerous studies aimed at demonstrating the future success of the project.",
-            "Kiruna kommun/White Arkitekter",
+            List.of(kirunaKommunStakeholder, whiteArkitekterStakeholder),
             "Design document",
             "1:7500",
             LocalDate.of(2014, 3, 17),
@@ -132,7 +142,7 @@ public class DataInitializer implements CommandLineRunner {
         Document document4 = new Document(null,
             "Deformation forecast",
             "The development plan shapes the form of the new city. The document, unlike previous competition documents, is written entirely in Swedish, which reflects the target audience: the citizens of Kiruna. The plan obviously contains many elements of the winning masterplan from the competition, some recommended by the jury, and others that were deemed appropriate to integrate later. The document is divided into four parts, with the third part, spanning 80 pages, describing the shape the new city will take and the strategies to be implemented for its relocation through plans, sections, images, diagrams, and texts. The document also includes numerous studies aimed at demonstrating the future success of the project.",
-            "LKAB",
+            List.of(lkabStakeholder),
             "Technical document",
             "1:12000",
             LocalDate.of(2014, 12, 1),
@@ -158,7 +168,7 @@ public class DataInitializer implements CommandLineRunner {
         Document document5 = new Document(null,
             "Adjusted development Plan",
             "This document is the update of the Development Plan, one year after its creation, modifications are made to the general master plan, which is published under the name Adjusted Development Plan91, and still represents the version used today after 10 years. Certainly, there are no drastic differences compared to the previous plan, but upon careful comparison, several modified elements stand out. For example, the central square now takes its final shape, as well as the large school complex just north of it, which appears for the first time.",
-            "Kiruna kommun/White Arkitekter",
+            List.of(kirunaKommunStakeholder, whiteArkitekterStakeholder),
             "Design document",
             "1:7500",
             LocalDate.of(2015, 1, 1),
@@ -184,7 +194,7 @@ public class DataInitializer implements CommandLineRunner {
         Document document6 = new Document(null,
             "Detail plan for square and commercial street",
             "This plan, approved in July 2016, is the first detailed plan to be implemented from the new masterplan (Adjusted development plan). The document defines the entire area near the town hall, comprising a total of 9 blocks known for their density. Among these are the 6 buildings that will face the main square. The functions are mixed, both public and private, with residential being prominent, as well as the possibility of incorporating accommodation facilities such as hotels. For all buildings in this plan, the only height limit is imposed by air traffic.",
-            "Kiruna kommun",
+            List.of(kirunaKommunStakeholder),
             "Prescriptive document",
             "1:1000",
             LocalDate.of(2016, 6, 22),
@@ -210,7 +220,7 @@ public class DataInitializer implements CommandLineRunner {
         Document document7 = new Document(null,
             "Construction of Scandic Hotel begins",
             "After two extensions of the land acquisition agreement, necessary because this document in Sweden is valid for only two years, construction of the hotel finally began in 2019.",
-            "LKAB",
+            List.of(lkabStakeholder),
             "Material effect",
             "Blueprint/Material effects",
             LocalDate.of(2019, 4, 19),
@@ -233,20 +243,12 @@ public class DataInitializer implements CommandLineRunner {
         );
         geoReferenceRepository.save(geoRef7);
 
-        /*
-        for (int i = 0; i < 100; i++) {
-            addNewDocument(i);
-        }
-        */
         documentLinkRepository.save(new DocumentLink(null, document1, document2, DocumentLinkType.DIRECT_CONSEQUENCE, LocalDateTime.now()));
         documentLinkRepository.save(new DocumentLink(null, document1, document2, DocumentLinkType.COLLATERAL_CONSEQUENCE, LocalDateTime.now()));
         documentLinkRepository.save(new DocumentLink(null, document1, document2, DocumentLinkType.PREVISION, LocalDateTime.now()));
         documentLinkRepository.save(new DocumentLink(null, document2, document3, DocumentLinkType.DIRECT_CONSEQUENCE, LocalDateTime.now()));
         documentLinkRepository.save(new DocumentLink(null, document2, document4, DocumentLinkType.COLLATERAL_CONSEQUENCE, LocalDateTime.now()));
         documentLinkRepository.save(new DocumentLink(null, document3, document4, DocumentLinkType.PREVISION, LocalDateTime.now()));
-
-        initializeStakeholders();
-        initializeDocumentType();
 
     }
 
@@ -255,7 +257,7 @@ public class DataInitializer implements CommandLineRunner {
         Document document = new Document(null,
             "Construction of Scandic Hotel begins - Document " + documentNumber,
             "After two extensions of the land acquisition agreement, necessary because this document in Sweden is valid for only two years, construction of the hotel finally began in 2019.",
-            "LKAB",
+            List.of(new Stakeholder("LKAB")),
             "Material effect",
             "Blueprint/Material effects",
             LocalDate.of(2019, 4, 19),
@@ -277,13 +279,6 @@ public class DataInitializer implements CommandLineRunner {
             geometryFactory.createPoint(new Coordinate(20.2500, 67.9000)) // Specific location
         );
         geoReferenceRepository.save(geoRef);
-    }
-
-    private void initializeStakeholders() {
-        stakeholderRepository.save(new Stakeholder(null, "Kiruna kommun"));
-        stakeholderRepository.save(new Stakeholder(null, "Residents"));
-        stakeholderRepository.save(new Stakeholder(null, "White Arkitekter"));
-        stakeholderRepository.save(new Stakeholder(null, "LKAB"));
     }
 
     private void initializeDocumentType() {
