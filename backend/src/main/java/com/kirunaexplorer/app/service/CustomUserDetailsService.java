600246f09de1e531c296807b7d242df727613b32
Nassim MANSOUR <mansournassim7@gmail.com>
2024-12-20 00:27:11 +0100

    Handle login

---
+++ backend/src/main/java/com/kirunaexplorer/app/service/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.kirunaexplorer.app.service;
+
+import com.kirunaexplorer.app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByUsername(username);
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new org.springframework.security.core.userdetails.User(user.get().
+                getUsername(), user.get().
+                getPassword(), new ArrayList<>());
+    }
+}
