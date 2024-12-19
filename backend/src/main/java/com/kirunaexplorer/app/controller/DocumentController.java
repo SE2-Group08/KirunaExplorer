380c39c845cc2aae7de27bae624ddc30f6da2280
Nassim MANSOUR <mansournassim7@gmail.com>
2024-12-20 00:37:17 +0100

    Minor improvement

--- backend/src/main/java/com/kirunaexplorer/app/controller/DocumentController.java
+++ backend/src/main/java/com/kirunaexplorer/app/controller/DocumentController.java
@@ -113,7 +113,6 @@ public class DocumentController {
      */
     @GetMapping("/diagram")
     public ResponseEntity<List<DocumentDiagramResponseDTO>> getDocumentsForDiagram() {
-        System.out.println("getDocumentsForDiagram");
         return ResponseEntity.ok(documentService.getDocumentsForDiagram());
     }
 }
