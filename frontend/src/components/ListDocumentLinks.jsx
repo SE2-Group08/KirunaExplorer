4e5f8868558de74bab85693d84d55733685fc083
Affamen <s322774@studenti.polito.it>
2025-01-06 05:37:56 +0100

    UI fix for document links list

--- frontend/src/components/ListDocumentLinks.jsx
+++ frontend/src/components/ListDocumentLinks.jsx
@@ -1,24 +1,19 @@
-
 import { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import { Button, Card, Row, Col, ListGroup } from "react-bootstrap";
 import API from "../API.mjs";
 import "../App.scss";
-import FeedbackContext  from "../contexts/FeedbackContext.js";
+import FeedbackContext from "../contexts/FeedbackContext.js";
+import { getIconUrlForDocument } from "../utils/iconMapping"; // Import the icon mapping utility
 
 const ListDocumentLinks = ({
-                             documentId,
-                             isOpen,
-                             onClose,
-                               authToken,
-                             // onSnippetClick,
-                             // document,
-                           }) => {
+  documentId,
+  isOpen,
+  onClose,
+  onSnippetClick,
+  authToken,
+}) => {
   const [snippets, setSnippets] = useState([]);
-  // const [showLinkModal, setShowLinkModal] = useState(false);
-  // const [selectedSnippet, setSelectedSnippet] = useState({});
-  // const [selectedLinkDocuments, setSelectedLinkDocuments] = useState([]);
-  // const [links, setLinks] = useState([]);
   const { setFeedbackFromError } = useContext(FeedbackContext);
 
   useEffect(() => {
@@ -33,98 +28,94 @@ const ListDocumentLinks = ({
     }
   }, [isOpen]);
 
-  // useEffect(() => {
-  //   if (!isOpen) {
-  //     setSnippets([]);
-  //   }
-  // }, [isOpen]);
-
-  // const handleSnippetClick = async (snippet) => {
-  //   setShowLinkModal(true);
-  //   const dLinks = await API.getAllLinksOfDocument(snippet.document.id);
-  //   setLinks(dLinks);
-  //   setSelectedSnippet(snippet);
-  // };
-
   return (
-      <div className={`slider ${isOpen ? "open" : ""}`}>
-        <div className="snippet-list">
-          <Row xs={1} className="g-4" style={{ width: "100%" }}>
-            <Card.Title>Linked documents</Card.Title>
-            {snippets.length ? (
-                snippets.map((snippet, index) => (
-                    <Col key={index}>
-                      <Card
-                          className="document-card slider-card"
-                          // onClick={() => handleSnippetClick(snippet)}
+    <div className={`slider ${isOpen ? "open" : ""}`}>
+      <button
+        type="button"
+        className="btn-close"
+        aria-label="Close"
+        onClick={onClose}
+        style={{ position: "absolute", top: "20px", right: "10px" }}
+      ></button>
+      <div className="snippet-list">
+        <Row xs={1} className="g-4" style={{ width: "100%" }}>
+          <Card.Title>Linked documents</Card.Title>
+          {snippets.length ? (
+            snippets.map((snippet, index) => (
+              <Col key={index}>
+                <Card className="document-card slider-card">
+                  <Card.Body>
+                    <Card.Title className="document-card-title">
+                      <img
+                        src={getIconUrlForDocument(
+                          snippet.document.type,
+                          snippet.document.stakeholders
+                        )}
+                        alt="Document Icon"
+                        className="me-2"
+                        style={{ width: "40px", height: "40px" }}
+                      />
+                      {snippet.document.title}
+                      <Button
+                        variant="outline-primary"
+                        size="sm"
+                        className="ms-2"
+                        onClick={() => onSnippetClick(snippet.document)}
+                        title="Open document"
+                        style={{ position: "absolute", top: "24px", right: "10px" }}
                       >
-                        <Card.Body>
-                          <Card.Title className="document-card-title">
-                            {snippet.document.title}
-                          </Card.Title>
-                          <div className="divider" />
-                          <Card.Text className="document-card-text">
-                            <strong>Scale:</strong> {snippet.document.scale}
-                          </Card.Text>
-                          <Card.Text className="document-card-text">
-                            <strong>Issuance Date:</strong>{" "}
-                            {snippet.document.issuanceDate}
-                          </Card.Text>
-                          <Card.Text className="document-card-text">
-                            <strong>Type:</strong> {snippet.document.type}
-                          </Card.Text>
-                          <Card.Text className="document-card-text">
-                            <strong>Stakeholders:</strong>{" "}
-                            {snippet.document.stakeholders.join(", ")}
-                          </Card.Text>
-                          <Card.Text className="document-card-text">
-                            <strong>Links:</strong>
-                          </Card.Text>
-                          <ListGroup>
-                            {snippet.links.map((link, idx) => (
-                                <ListGroup.Item key={idx}>
-                                  {link.linkType
-                                      .toLowerCase()
-                                      .replace(
-                                          /_([a-z])/g,
-                                          (match, letter) => ` ${letter.toUpperCase()}`
-                                      )
-                                      .replace(/^\w/, (c) => c.toUpperCase())}
-                                </ListGroup.Item>
-                            ))}
-                          </ListGroup>
-                        </Card.Body>
-                      </Card>
-                    </Col>
-                ))
-            ) : (
-                <Col>
-                  <div>
-                    <p>This document has not been linked yet.</p>
-                    <p>
-                      Once you will have linked it to another document you will be
-                      able to see here the type of link
-                    </p>
-                  </div>
-                </Col>
-            )}
-          </Row>
-        </div>
-        <Button variant="secondary" onClick={onClose} className="close-button">
-          Close
-        </Button>
-
-        {/* {showLinkModal && (
-        <LinkModal
-          showModal={showLinkModal}
-          handleClose={() => setShowLinkModal(false)}
-          document={selectedSnippet}
-          links={links}
-          selectedDocumentToLink={document}
-          setSelectedLinkDocuments={setSelectedLinkDocuments}
-        />
-      )} */}
+                        <i className="bi bi-eye"></i>
+                      </Button>
+                    </Card.Title>
+                    <div className="divider" />
+                    <Card.Text className="document-card-text">
+                      <strong>Scale:</strong> {snippet.document.scale}
+                    </Card.Text>
+                    <Card.Text className="document-card-text">
+                      <strong>Issuance Date:</strong>{" "}
+                      {snippet.document.issuanceDate}
+                    </Card.Text>
+                    <Card.Text className="document-card-text">
+                      <strong>Type:</strong> {snippet.document.type}
+                    </Card.Text>
+                    <Card.Text className="document-card-text">
+                      <strong>Stakeholders:</strong>{" "}
+                      {snippet.document.stakeholders.join(", ")}
+                    </Card.Text>
+                    <Card.Text className="document-card-text">
+                      <strong>Links:</strong>
+                    </Card.Text>
+                    <ListGroup>
+                      {snippet.links.map((link, idx) => (
+                        <ListGroup.Item key={idx}>
+                          {link.linkType
+                            .toLowerCase()
+                            .replace(
+                              /_([a-z])/g,
+                              (match, letter) => ` ${letter.toUpperCase()}`
+                            )
+                            .replace(/^\w/, (c) => c.toUpperCase())}
+                        </ListGroup.Item>
+                      ))}
+                    </ListGroup>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))
+          ) : (
+            <Col>
+              <div>
+                <p>This document has not been linked yet.</p>
+                <p>
+                  Once you will have linked it to another document you will be
+                  able to see here the type of link
+                </p>
+              </div>
+            </Col>
+          )}
+        </Row>
       </div>
+    </div>
   );
 };
 ListDocumentLinks.propTypes = {
